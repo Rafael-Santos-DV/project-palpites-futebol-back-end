e1ed7275ec6449ef6fe8370ffdfdd37eec58158c
Rafael Santos <santosrafaell187@gmail.com>
2022-02-20 19:15:41 -0300

    removido origin cors

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.URL_MONGODB, error => {
 })
 
 app.use(morgan("dev"));
-app.use(cors({ origin: "https://xenodochial-curran-3baef9.netlify.app/" }));
+app.use(cors());
 app.use(express.json());
 app.use(require("./routes/routes"));
 
