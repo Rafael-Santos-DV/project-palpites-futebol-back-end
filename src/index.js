3d5365abb639ccdbd0d0fbafa3af652982dfb7e0
Rafael Santos <santosrafaell187@gmail.com>
2022-02-20 19:52:18 -0300

    function heades add

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ mongoose.connect(process.env.URL_MONGODB, error => {
 
 app.use(morgan("dev"));
 app.use(cors({ origin: "https://xenodochial-curran-3baef9.netlify.app" }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://xenodochial-curran-3baef9.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 app.use(require("./routes/routes"));
 
