3c2dea7942db83ef3eaf64910eea69f4447d1bfe
Rafael Santos <santosrafaell187@gmail.com>
2022-02-20 20:35:05 -0300

    cors permiçao add

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ mongoose.connect(process.env.URL_MONGODB, error => {
 })
 
 app.use(morgan("dev"));
-app.use(cors({ origin: "http://xenodochial-curran-3baef9.netlify.app" }));
+
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://xenodochial-curran-3baef9.netlify.app");
+    res.header("Access-Control-Allow-Origin", "https://xenodochial-curran-3baef9.netlify.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    app.use(cors());
     next();
+
 });
 
 app.use(express.json());
