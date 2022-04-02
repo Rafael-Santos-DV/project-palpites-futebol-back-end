9b47b1d566a31e8623b16988675d93e3af1dc4de
Rafael Santos <90205772+Rafael-Santos-DV@users.noreply.github.com>
2022-04-02 16:12:12 -0300

    update cors

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://xenodochial-curran-3baef9.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    app.use(cors({ origin: "https://xenodochial-curran-3baef9.netlify.app" }));
+    app.use(cors({ origin: "https://palpites-futebol.netlify.app" }));
     next();
 
 });
