bd603503b361c82ec49d9b0468f56267b01819f8
Rafael Santos <90205772+Rafael-Santos-DV@users.noreply.github.com>
2022-04-02 16:13:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.URL_MONGODB, error => {
 app.use(morgan("dev"));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://xenodochial-curran-3baef9.netlify.app");
+    res.header("Access-Control-Allow-Origin", "https://palpites-futebol.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     app.use(cors({ origin: "https://palpites-futebol.netlify.app" }));
     next();
